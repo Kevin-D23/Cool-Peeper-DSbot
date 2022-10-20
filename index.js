e8468dd1cf5e7603f9e3c12e015e090c462e474c
Kevin <kevinduong111@gmail.com>
2022-10-20 12:54:11 -0700

    set presence

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const client = new Client({
 });
 
 client.on('ready', () => {
-  console.log(`Logg in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user?.setPresence({status: 'dnd', activities: [{name:  "with myself"}]})
 
   setInterval(checkBirthday, 60000);
 });
