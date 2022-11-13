c70e7d0300ad76784521db66ffc1fbf725843df3
Kevin <kevinduong111@gmail.com>
2022-11-12 16:08:37 -0800

    bug fixed

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const rest = new REST({version: '10'}).setToken(process.env.TOKEN)
 
 async function main() {
   try {
-    await rest.put(Routes.applicationGuildCommands(process.env.clientID, process.env.testServerID), {
+    await rest.put(Routes.applicationGuildCommands(process.env.clientID, process.env.guildID), {
       body: commands,
     }) 
   } catch(err) {
