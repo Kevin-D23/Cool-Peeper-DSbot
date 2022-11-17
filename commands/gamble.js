5241d7c3d0a1c94a295e02c95ddc2f4c72ed9ade
Kevin <kevinduong111@gmail.com>
2022-11-16 20:22:29 -0800

    coinstats formatting

--- commands/gamble.js
+++ commands/gamble.js
@@ -78,7 +78,7 @@ async function coinStats() {
     const tails = result.money
     const headsPerc = ((heads / (heads + tails)) * 100).toFixed(1)
     const tailsPerc = 100 - headsPerc
-    return 'Heads-> ' + heads + ' %' + headsPerc + "\nTails---> " + tails + ' %' + tailsPerc
+    return 'Heads: ' + heads + ' ' + headsPerc + "%\nTails: " + tails + ' ' + tailsPerc + '%'
 }
 
 async function leaderboardUsers() {
