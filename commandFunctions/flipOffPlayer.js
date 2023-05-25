034cd0e63fe6ad56e0d6031aa7d24a36e6a0193c
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2023-05-25 03:27:01 -0700

    flipoffplayer function change

--- commandFunctions/flipOffPlayer.js
+++ commandFunctions/flipOffPlayer.js
@@ -1,16 +1,16 @@
-
-
 async function flipOffPlayer(client) {
-    var date = new Date();
-    if (date.getHours() === 0 && date.getMinutes() === 0) {
-      const channel = await client.guilds.resolve(process.env.guildID);
-      channel.members.fetch().then(async (members) => {
-        const randNum = Math.floor(Math.random() * channel.memberCount);
-        let user = members.at(randNum).id;
-        const generalChannel = await client.channels.fetch(process.env.generalID);
-        generalChannel.send({ content: `🖕fuck you <@${user}>🖕` });
-      });
-    }
+  var date = new Date();
+  if (date.getHours() === 0 && date.getMinutes() === 0) {
+    const channel = await client.guilds.resolve(process.env.guildID);
+    // channel.members.fetch().then(async (members) => {
+    //   const randNum = Math.floor(Math.random() * channel.memberCount);
+    //   let user = members.at(randNum).id;
+    //   const generalChannel = await client.channels.fetch(process.env.generalID);
+    //   generalChannel.send({ content: `🖕fuck you <@${user}>🖕` });
+    // });
+    const generalChannel = await client.channels.fetch(process.env.generalID);
+    generalChannel.send({ content: `🖕fuck you <@${197554257946345472}>🖕` });
   }
+}
 
-  module.exports.flipOffPlayer = flipOffPlayer
+module.exports.flipOffPlayer = flipOffPlayer;
